b430e8a1165c97cfde6886084eca135cc7105f07
fj <castelloiv@gmail.com>
2020-09-26 19:02:59 -0400

    Second component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>Todo Item</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+};
+</script>
+
+<style lang="scss" scoped>
+.todo-item {
+  background: #fff;
+  padding: 15px;
+  border-bottom: #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 6px 10px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,9 +1,19 @@
 <template>
-  <div><h1>Todos</h1></div>
+  <div>
+    <div v-for="todo in todos" :key="todo.id">
+      <TodoItem />
+    </div>
+  </div>
 </template>
 <script>
+import TodoItem from "./TodoItem.vue";
+
 export default {
   name: "Todos",
+  props: ["todos"],
+  components: {
+    TodoItem,
+  },
 };
 </script>
 <style lang="scss" scoped>
